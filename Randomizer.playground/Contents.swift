1d0fbb186913225cd9824456eb06468a780a9af0
Andrew Porter <haporter@me.com>
2015-11-18 15:47:48 -0700

    starting on views
    Known bugs
    - Will not make pairs when array has an odd number

--- Randomizer.playground/Contents.swift
+++ Randomizer.playground/Contents.swift
@@ -2,30 +2,101 @@
 
 import UIKit
 
-func randomIndex(range: Int) {
-    let randomNumber: Int = Int(arc4random_uniform(UInt32(range + 1)))
-    
-    print("\(randomNumber)")
-    
-}
+// For some reason this wasn't giving me random numbers
+//func randomIndex(range: Int) {
+//    let randomNumber: Int = Int(arc4random_uniform(UInt32(range)))
+//    
+//    print("\(randomNumber)")
+//    
+//}
 
 func randomizer(nameArray: [String]) -> [String:String] {
     
     // Array minus the already used names
     var updatedArray = nameArray
+    print(updatedArray.count)
     
     // A dictionary to hold the pairs
     var pairsDict: [String:String] = [ : ]
     
     // check to see if the array of names is more than 3
-    // If it is less than 3 "Can't assign pairs less than 3"
-    // iterate through the updatedArray and pick two random names
-    // if the two random names are the same pick a new random name until they are different
-    // remove the used names from the array
- 
+    // check to see if the array is even
+    // check to see if the array is odd
+    // iterate through the updatedArray and pick one randome name
+    // remove that name from the array
+    // pick next random name
+    // remove that name from array
+        
+    if updatedArray.count % 2 == 0 {
+        print("even number")
+        
+        for _ in updatedArray {
     
+            var randIndex = Int(arc4random_uniform(UInt32(updatedArray.count)))
+            let name1 = updatedArray[randIndex]
+            print(name1)
+            updatedArray.removeAtIndex(randIndex)
+            print(updatedArray.count)
+            
+            randIndex = Int(arc4random_uniform(UInt32(updatedArray.count)))
+            let name2 = updatedArray[randIndex]
+            print(name2)
+            updatedArray.removeAtIndex(randIndex)
+            print(updatedArray.count)
+            
+            pairsDict[name1] = name2
+            if updatedArray.count == 0 {
+                break
+            }
+        }
+        
+    }
     
+    if updatedArray.count % 3 == 0 {
+        print("odd number")
+        
+        if updatedArray.count > 1 {
+            
+            for _ in updatedArray {
+                
+                var randIndex = Int(arc4random_uniform(UInt32(updatedArray.count)))
+                let name1 = updatedArray[randIndex]
+                updatedArray.removeAtIndex(randIndex)
+                print(updatedArray.count)
+                
+                randIndex = Int(arc4random_uniform(UInt32(updatedArray.count)))
+                let name2 = updatedArray[randIndex]
+                updatedArray.removeAtIndex(randIndex)
+                print(updatedArray.count)
+                
+                pairsDict[name1] = name2
+                if updatedArray.count == 0 {
+                    break
+                }
+            }
+
+        } else {
+            
+            print("\(updatedArray[0]) will have to join a pair to make a threesome")
+            
+        }
+    }
+    
+    print("I made it here")
     return pairsDict
 }
+
+
+randomizer(["Andrew","Porter","Libby","Hyrum","Ty","Mike","Cory","Andy","Scott","Miles","greg"])
+
+
+
+
+
+
+
+
+
+
 
 
